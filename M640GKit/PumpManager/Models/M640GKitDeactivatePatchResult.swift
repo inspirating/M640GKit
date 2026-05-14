cafac7037804d817792afd7f9d1cb5e6ba1722e2
inspirating <inspirating@hotmail.com>
2026-05-15 07:16:15 +0800

    delete rebundent

--- M640GKit/PumpManager/Models/M640GKitDeactivatePatchResult.swift
+++
@@ -1,9 +0,0 @@
-public enum M640GKitDeactivatePatchResult {
-    case success
-    case failure(error: M640GKitDeactivatePatchError)
-}
-
-public enum M640GKitDeactivatePatchError: LocalizedError {
-    case connectionFailure
-    case unknownError(reason: String)
-}
--- M640GKit/PumpManager/Models/M640GKitUpdatePatchResult.swift
+++
@@ -1,9 +0,0 @@
-public enum M640GKitUpdatePatchResult {
-    case success
-    case failure(error: M640GKitUpdatePatchError)
-}
-
-public enum M640GKitUpdatePatchError: LocalizedError {
-    case connectionFailure
-    case unknownError(reason: String)
-}
